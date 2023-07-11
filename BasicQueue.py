96ffb95f9b9ddec829b44a966ea7cc3c54021f8b
kaulas12 <kaulasdevyani12@gmail.com>
2023-07-11 17:18:34 +0530

    Basic2

--- BasicQueue.py
+++ BasicQueue.py
@@ -16,6 +16,25 @@ class Queue:
         self.items.append(values)
         return "Yes the item is inserted"
     
+    def dequeue(self):
+        if self.isEmpty():
+            return 'Queue is empty'
+        else:
+            return self.items.pop(0)
+    
+    def peek(self):
+        if self.isEmpty():
+            return 'Queue is empty'
+        else:
+            return f'{self.items[0]}'
+    
+    def delete(self):
+        self.items = None
+    
 customQueue = Queue()
 customQueue.enqueue(1)
+customQueue.enqueue(2)
+customQueue.enqueue(3)
 print(customQueue)
+# print(customQueue.dequeue())
+print(customQueue.peek())
