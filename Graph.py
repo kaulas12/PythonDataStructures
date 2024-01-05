3bd6de5ac76fd2f2cf4159f15fc23786db0dbf38
Devyani Kaulas <65672547+kaulas12@users.noreply.github.com>
2024-01-05 17:18:04 +0530

    Update Graph.py
    Consists of application of Graph through Adjacency List using Python Dictionaries. Have operations : Creating AdjList, AddVertex, AddEdge,RemoveVertex and Remove Edge

--- Graph.py
+++ Graph.py
@@ -15,6 +15,20 @@ class Graph:
             return True
         return False
     
+    def removeEdge(self,vertex1,vertex2):
+        if vertex1 in self.adjacencyList.keys() and vertex2 in self.adjacencyList.keys():
+            self.adjacencyList[vertex1].remove(vertex2)
+            self.adjacencyList[vertex2].remove(vertex1)
+            return True
+        return False
+            
+    def removeVertex(self,vertex):
+        if vertex in self.adjacencyList.keys():
+            for other_vertex in self.adjacencyList[vertex]:
+                self.adjacencyList[other_vertex].remove(vertex)
+            del self.adjacencyList[vertex]
+        return True
+            
     def printGraph(self):
         for vertex in self.adjacencyList:
             print(vertex,":",self.adjacencyList[vertex])
@@ -23,5 +37,15 @@ class Graph:
 customGraph = Graph()
 customGraph.addVertex("A")
 customGraph.addVertex("B")
+customGraph.addVertex("C")
+customGraph.addVertex("D")
 customGraph.addEdge("A","B")
+customGraph.addEdge("C","B")
+customGraph.addEdge("A","C")
+customGraph.addEdge("D","C")
+customGraph.addEdge("A","D")
+customGraph.printGraph()
+# customGraph.removeEdge("A","C")
+print('-----------------------------------------------')
+customGraph.removeVertex("D")
 customGraph.printGraph()
