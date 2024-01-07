dc1ede120797c1a48cd7a30445208a0d7490c02c
Devyani Kaulas <65672547+kaulas12@users.noreply.github.com>
2024-01-07 16:26:08 +0530

    Create BreadthFirstSearch.py
    Because of the pop method, the time complexity will become O(n), so dequeue will be used

---
+++ BreadthFirstSearch.py
@@ -0,0 +1,64 @@
+class Graph:
+    def __init__(self):
+        self.adjacencyList = {}
+        
+    def printGraph(self):
+        for vertex in self.adjacencyList:
+            print(vertex,":",self.adjacencyList[vertex])
+    
+    def addVertex(self,vertex):
+        if vertex not in self.adjacencyList:
+            self.adjacencyList[vertex] = []
+            return True
+        return False
+    
+    def addEdge(self,vertex1,vertex2):
+        if vertex1 in self.adjacencyList.keys() and vertex2 in self.adjacencyList.keys():
+            self.adjacencyList[vertex1].append(vertex2)
+            self.adjacencyList[vertex2].append(vertex1)
+            return True
+        return False
+    
+    def removeEdge(self,vertex1,vertex2):
+        if vertex1 in self.adjacencyList.keys() and vertex2 in self.adjacencyList.keys():
+            self.adjacencyList[vertex1].remove(vertex2)
+            self.adjacencyList[vertex2].remove(vertex1)
+            return True
+        return False
+            
+    def removeVertex(self,vertex):
+        if vertex in self.adjacencyList.keys():
+            for other_vertex in self.adjacencyList[vertex]:
+                self.adjacencyList[other_vertex].remove(vertex)
+            del self.adjacencyList[vertex]
+        return True
+    
+    def bfs(self,vertex):
+        visited = set()
+        visited.add(vertex)
+        queue = [vertex]
+        while queue:
+            current_vertex = queue.pop(0)
+            print(f'{current_vertex}')
+            for adjVertex in self.adjacencyList[current_vertex]:
+                if adjVertex not in visited:
+                    visited.add(adjVertex)
+                    queue.append(adjVertex)
+                    
+            
+    
+            
+            
+customGraph = Graph()
+customGraph.addVertex("A")
+customGraph.addVertex("B")
+customGraph.addVertex("C")
+customGraph.addVertex("D")
+customGraph.addVertex("E")
+customGraph.addEdge("A","B")
+customGraph.addEdge("E","B")
+customGraph.addEdge("A","C")
+customGraph.addEdge("D","E")
+customGraph.addEdge("C","D")
+customGraph.printGraph()
+customGraph.bfs("A")
